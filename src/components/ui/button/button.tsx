d28374618da6b5550b99ed1b65dfdbe08055a1db
MalevichDi2910 <malevicdiana@gmail.com>
2023-11-11 00:07:35 +0200

    add polymorphic button

---
+++ src/components/ui/button/button.tsx
@@ -0,0 +1,22 @@
+import { ComponentPropsWithoutRef, ElementType } from 'react'
+
+export type ButtonProps<T extends ElementType = 'button'> = {
+  as?: T
+  variant?: 'primary' | 'secondary' | 'tertiary' | 'link'
+  fullWidth?: boolean
+  className?: string
+} & ComponentPropsWithoutRef<T>
+
+import s from './button.module.scss'
+
+export const Button = <T extends ElementType = 'button'>(
+  props: ButtonProps<T> & Omit<ComponentPropsWithoutRef<T>, keyof ButtonProps<T>>
+) => {
+  const { variant = 'primary', fullWidth, className, as: Component = 'button', ...rest } = props
+  return (
+    <Component
+      className={`${s.button} ${s[variant]} ${fullWidth ? s.fullWidth : ''} ${className}`}
+      {...rest}
+    />
+  )
+}

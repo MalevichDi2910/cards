963eb247c618261eb1bd6d163f28ce2620731733
MalevichDi2910 <malevicdiana@gmail.com>
2023-11-13 16:29:10 +0200

    edit polymorphic button

--- src/components/ui/button/button.stories.ts
+++ src/components/ui/button/button.stories.ts
@@ -11,7 +11,7 @@ const meta = {
   },
   component: Button,
   tags: ['autodocs'],
-  title: 'Components/Button',
+  title: 'Components/UI/Button',
 } satisfies Meta<typeof Button>
 
 export default meta
@@ -49,10 +49,9 @@ export const Link: Story = {
 
 export const FullWidth: Story = {
   args: {
+    ...Primary.args,
     children: 'Full Width Button',
-    disabled: false,
     fullWidth: true,
-    variant: 'primary',
   },
 }
 
--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -1,5 +1,9 @@
 import { ComponentPropsWithoutRef, ElementType } from 'react'
 
+import { clsx } from 'clsx'
+
+import s from './button.module.scss'
+
 export type ButtonProps<T extends ElementType = 'button'> = {
   as?: T
   className?: string
@@ -7,17 +11,11 @@ export type ButtonProps<T extends ElementType = 'button'> = {
   variant?: 'link' | 'primary' | 'secondary' | 'tertiary'
 } & ComponentPropsWithoutRef<T>
 
-import s from './button.module.scss'
-
 export const Button = <T extends ElementType = 'button'>(
   props: ButtonProps<T> & Omit<ComponentPropsWithoutRef<T>, keyof ButtonProps<T>>
 ) => {
   const { as: Component = 'button', className, fullWidth, variant = 'primary', ...rest } = props
+  const classNames = clsx(s[variant], className, fullWidth && s.fullWidth, s.button)
 
-  return (
-    <Component
-      className={`${s.button} ${s[variant]} ${fullWidth ? s.fullWidth : ''} ${className}`}
-      {...rest}
-    />
-  )
+  return <Component className={classNames} {...rest} />
 }

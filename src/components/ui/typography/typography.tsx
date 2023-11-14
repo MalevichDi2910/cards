2a984feffa3490559c91dd127a4238b6cfc40487
MalevichDi2910 <malevicdiana@gmail.com>
2023-11-14 17:45:14 +0200

    added typography

---
+++ src/components/ui/typography/typography.tsx
@@ -0,0 +1,33 @@
+import { ComponentPropsWithoutRef, ElementType, ReactNode } from 'react'
+
+import { clsx } from 'clsx'
+
+import s from './typography.module.scss'
+
+export type TypographyProps<T extends ElementType = 'span'> = {
+  as?: T
+  children?: ReactNode
+  className?: string
+  variant:
+    | 'body1'
+    | 'body2'
+    | 'caption'
+    | 'h1'
+    | 'h2'
+    | 'h3'
+    | 'large'
+    | 'link1'
+    | 'link2'
+    | 'overline'
+    | 'subtitle1'
+    | 'subtitle2'
+} & ComponentPropsWithoutRef<T>
+
+export const Typography = <T extends ElementType = 'span'>(
+  props: TypographyProps<T> & Omit<ComponentPropsWithoutRef<T>, keyof TypographyProps<T>>
+) => {
+  const { as: Component = 'span', className, variant, ...rest } = props
+  const classNames = clsx(s[variant], className)
+
+  return <Component className={classNames} {...rest} />
+}

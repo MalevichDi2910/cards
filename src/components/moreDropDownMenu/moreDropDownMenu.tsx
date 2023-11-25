ea0ca9bdd47d099891976a10ff34f111d1f8689b
Ramin Mamedov <mamedov10rm@mail.ru>
2023-11-25 22:20:20 +0400

    add moreDropDownMenu.tsx, moreDropDownMenu.module.scss, userDropDownMenu.tsx

---
+++ src/components/moreDropDownMenu/moreDropDownMenu.tsx
@@ -0,0 +1,37 @@
+import { DeleteIcon } from '@/assets/icons/deleteIcon'
+import { EditIcon } from '@/assets/icons/editIcon'
+import { MoreInfoIcon } from '@/assets/icons/moreInfoIcon'
+import { PlayIcon } from '@/assets/icons/playIcon'
+
+import s from '../moreDropDownMenu/moreDropDownMenu.module.scss'
+
+import { DropDownItem, DropDownMenu, DropDownSeparator } from '../ui/dropDownMenu'
+
+export const MoreDropDownMenu = () => {
+  return (
+    <div className={s.wrapper}>
+      <DropDownMenu trigger={<MoreInfoIcon />}>
+        <DropDownItem asChild>
+          <button className={s.button} onClick={() => {}}>
+            <PlayIcon className={s.icon} color={'white'} height={'16'} width={'16'} />
+            Learn
+          </button>
+        </DropDownItem>
+        <DropDownSeparator />
+        <DropDownItem asChild>
+          <button className={s.button} onClick={() => {}}>
+            <EditIcon className={s.icon} color={'white'} height={'16'} width={'16'} />
+            Edit
+          </button>
+        </DropDownItem>
+        <DropDownSeparator />
+        <DropDownItem asChild>
+          <button className={s.button} onClick={() => {}}>
+            <DeleteIcon className={s.icon} color={'white'} height={'16'} width={'16'} />
+            Delete
+          </button>
+        </DropDownItem>
+      </DropDownMenu>
+    </div>
+  )
+}

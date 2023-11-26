6777b47562d7ed3618c8d97f008357ef75db994a
Ramin Mamedov <mamedov10rm@mail.ru>
2023-11-26 23:32:57 +0400

    fix moreDropDownMenu

--- src/components/moreDropDownMenu/moreDropDownMenu.tsx
+++ src/components/moreDropDownMenu/moreDropDownMenu.tsx
@@ -1,7 +1,7 @@
-import { DeleteIcon } from '@/assets/icons/deleteIcon'
-import { EditIcon } from '@/assets/icons/editIcon'
-import { MoreInfoIcon } from '@/assets/icons/moreInfoIcon'
-import { PlayIcon } from '@/assets/icons/playIcon'
+import DeleteIcon from '@/assets/icons/deleteIcon'
+import EditIcon from '@/assets/icons/editIcon'
+import MoreInfoIcon from '@/assets/icons/moreInfoIcon'
+import PlayIcon from '@/assets/icons/playIcon'
 
 import s from '../moreDropDownMenu/moreDropDownMenu.module.scss'
 
@@ -10,24 +10,24 @@ import { DropDownItem, DropDownMenu, DropDownSeparator } from '../ui/dropDownMen
 export const MoreDropDownMenu = () => {
   return (
     <div className={s.wrapper}>
-      <DropDownMenu trigger={<MoreInfoIcon />}>
+      <DropDownMenu trigger={<MoreInfoIcon color={'white'} height={24} width={24} />}>
         <DropDownItem asChild>
           <button className={s.button} onClick={() => {}}>
-            <PlayIcon className={s.icon} color={'white'} height={'16'} width={'16'} />
+            <PlayIcon className={s.icon} color={'white'} height={16} width={16} />
             Learn
           </button>
         </DropDownItem>
         <DropDownSeparator />
         <DropDownItem asChild>
           <button className={s.button} onClick={() => {}}>
-            <EditIcon className={s.icon} color={'white'} height={'16'} width={'16'} />
+            <EditIcon className={s.icon} color={'white'} height={16} width={16} />
             Edit
           </button>
         </DropDownItem>
         <DropDownSeparator />
         <DropDownItem asChild>
           <button className={s.button} onClick={() => {}}>
-            <DeleteIcon className={s.icon} color={'white'} height={'16'} width={'16'} />
+            <DeleteIcon className={s.icon} color={'white'} height={16} width={16} />
             Delete
           </button>
         </DropDownItem>

ea3872eba53f117059f21dfa036566ae2a95b1ee
MalevichDi2910 <malevicdiana@gmail.com>
2023-12-01 23:54:16 +0200

    feat: added Pagination component (completed)

--- src/components/ui/pagination/pagination.tsx
+++ src/components/ui/pagination/pagination.tsx
@@ -4,6 +4,7 @@ import { MainPaginationButtons } from '@/components/ui/pagination/mainPagination
 import { NextButton, PrevButton } from '@/components/ui/pagination/navigationButtons'
 import { PerPageSelect } from '@/components/ui/pagination/perPageSelect'
 import { usePagination } from '@/components/ui/pagination/usePagination'
+import { Option } from '@/components/ui/select'
 import axios from 'axios'
 
 import s from './pagination.module.scss'
@@ -12,9 +13,10 @@ export type PaginationProps = {
   children?: ReactNode
   count: number
   onChange: (page: number) => void
-  onPerPageChange?: (perPage: number) => void
+  onPerPageChange?: (itemPerPage: number) => void
   page: number
-  perPage?: string
+  perPage?: number
+  perPageOptions?: Option[]
   siblings?: number
 }
 
@@ -26,20 +28,14 @@ export type CardPropsType = {
 }
 
 const selectItems = [
-  { title: '10', value: 'ten' },
-  { title: '20', value: 'twenty' },
-  { title: '30', value: 'thirty' },
-  { title: '50', value: 'forty' },
-  { title: '100', value: 'one hundred' },
+  { title: '10', value: '10' },
+  { title: '20', value: '20' },
+  { title: '30', value: '30' },
+  { title: '50', value: '50' },
+  { title: '100', value: '100' },
 ]
 
-export const Pagination: FC<PaginationProps> = ({
-  count,
-  onChange,
-  page,
-  // perPage,
-  siblings,
-}) => {
+export const Pagination: FC<PaginationProps> = ({ count, onChange, page, siblings }) => {
   const {
     handleMainPageClicked,
     handleNextPageClicked,
@@ -91,11 +87,7 @@ export const Pagination: FC<PaginationProps> = ({
           paginationRange={paginationRange}
         />
         <NextButton disabled={isLastPage} onClick={handleNextPageClicked} />
-        <PerPageSelect
-          onPerPageChange={onPerPageChangeHandler}
-          perPage={cardsPerPage}
-          perPageOptions={selectItems}
-        />
+        <PerPageSelect onPerPageChange={onPerPageChangeHandler} perPageOptions={selectItems} />
       </div>
       <div>
         <ul>

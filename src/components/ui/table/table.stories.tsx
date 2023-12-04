0e3ec71f35883a15bb3771f3996181aab9618a34
Yuliya Martinchuk <yuliyamartinchuk@gmail.com>
2023-12-05 01:03:21 +0300

    feat: create story columnsFriendsPack for Table component

--- src/components/ui/table/table.stories.tsx
+++ src/components/ui/table/table.stories.tsx
@@ -7,6 +7,7 @@ import { Edit } from '@/assets/icons/edit'
 import { Delete } from '@/assets/icons/delete'
 import { Column, TableHeader } from '@/components/ui/table/tableHeader'
 import { Typography } from '@/components/ui/typography'
+import { Rating } from '@/components/ui/rating'
 
 const meta: Meta<typeof Table.Root> = {
   component: Table.Root,
@@ -26,7 +27,7 @@ const iconsTableWrapper: CSSProperties = {
   padding: '6px 24px',
 }
 
-const columnsDefault: Column[] = [
+const columnsDefaultPackList: Column[] = [
   {
     key: 'name',
     title: 'Name',
@@ -49,7 +50,7 @@ const columnsDefault: Column[] = [
   },
 ]
 
-const data = [
+const dataPackList = [
   {
     title: 'Pack Name 1',
     cardsCount: 4,
@@ -112,22 +113,108 @@ const data = [
   },
 ]
 
-const columns: Column[] = [
-  ...columnsDefault,
-  {
-    key: 'icons',
-    title: '',
-  },
-]
-
 export const PackListTable: Story = {
   args: {
     children: (
       <>
-        <TableHeader columns={columns} />
+        <TableHeader columns={columnsDefaultPackList} />
         <Table.Body>
-          {data.map(item => (
+          {dataPackList.map(item => (
             <Table.Row key={item.title}>
+              {Object.values(item).map((value, index) => {
+                return (
+                  <Table.Cell key={`${value}${index}`}>
+                    <Typography variant={'body2'}>{value}</Typography>
+                  </Table.Cell>
+                )
+              })}
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </>
+    ),
+  },
+  render: args => <Table.Root {...args} />,
+}
+
+const columnsFriendsPack: Column[] = [
+  {
+    key: 'question',
+    title: 'Question',
+  },
+  {
+    key: 'answer',
+    title: 'Answer',
+  },
+  {
+    key: 'lastUpdated',
+    title: 'Last Updated',
+  },
+  {
+    key: 'grade',
+    title: 'Grade',
+  },
+]
+
+const dataFriendsPack = [
+  {
+    question: 'How "This" works in JavaScript?',
+    answer: 'This is how "This" works in JavaScript',
+    lastUpdated: '18.01.23',
+    grade: {
+      ...(
+        <>
+          <Rating rating={4} />
+        </>
+      ),
+    },
+  },
+  {
+    question: 'How "This" works in JavaScript?',
+    answer: 'This is how "This" works in JavaScript',
+    lastUpdated: '07.01.23',
+    grade: {
+      ...(
+        <>
+          <Rating rating={2} />
+        </>
+      ),
+    },
+  },
+  {
+    question: 'How "This" works in JavaScript?',
+    answer: 'This is how "This" works in JavaScript',
+    lastUpdated: '08.04.23',
+    grade: {
+      ...(
+        <>
+          <Rating rating={3} />
+        </>
+      ),
+    },
+  },
+  {
+    question: 'How "This" works in JavaScript?',
+    answer: 'This is how "This" works in JavaScript',
+    lastUpdated: '11.05.23',
+    grade: {
+      ...(
+        <>
+          <Rating rating={5} />
+        </>
+      ),
+    },
+  },
+]
+
+export const FriendsPackTable: Story = {
+  args: {
+    children: (
+      <>
+        <TableHeader columns={columnsFriendsPack} />
+        <Table.Body>
+          {dataFriendsPack.map(item => (
+            <Table.Row key={item.question}>
               {Object.values(item).map((value, index) => {
                 return (
                   <Table.Cell key={`${value}${index}`}>

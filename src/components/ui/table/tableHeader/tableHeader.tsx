4d86f363fecc822f283390f30dc56ef7c2dd7518
Yuliya Martinchuk <yuliyamartinchuk@gmail.com>
2023-12-04 21:53:17 +0300

    style: add style for TableHeader component

--- src/components/ui/table/tableHeader/tableHeader.tsx
+++ src/components/ui/table/tableHeader/tableHeader.tsx
@@ -4,6 +4,8 @@ import { ArrowIosDown } from '@/assets/icons/arrowIosDown'
 import { Table } from '@/components/ui/table'
 import { Typography } from '@/components/ui/typography'
 import { ArrowIosUp } from '@/assets/icons/arrowIosUp'
+import { clsx } from 'clsx'
+import s from './TableHeader.module.scss'
 
 export type Column = {
   key: string
@@ -50,14 +52,24 @@ export const TableHeader = forwardRef<ElementRef<typeof Table.Head>, Props>(
       <Table.Head ref={ref} {...restProps}>
         <Table.Row>
           {columns.map(({ key, sortable, title }) => {
+            const headCellClassName = clsx(sortable && s.activeHeadCell)
+
             return (
-              <Table.HeadCell key={key} onClick={handleSort(key, sortable)}>
-                <Typography as={'span'} variant={'subtitle2'}>
+              <Table.HeadCell
+                className={headCellClassName}
+                key={key}
+                onClick={handleSort(key, sortable)}
+              >
+                <Typography as={'span'} className={s.sortCell} variant={'subtitle2'}>
                   {title}
                   {sort && sort.key === key && (
                     <>
-                      {sort.direction === 'asc' && <ArrowIosUp size={0.75} />}
-                      {sort.direction !== 'asc' && <ArrowIosDown size={0.75} />}
+                      {sort.direction === 'asc' && (
+                        <ArrowIosUp className={s.sortIcon} size={0.75} />
+                      )}
+                      {sort.direction !== 'asc' && (
+                        <ArrowIosDown className={s.sortIcon} size={0.75} />
+                      )}
                     </>
                   )}
                 </Typography>

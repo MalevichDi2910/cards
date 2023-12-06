1d86557d16da8c861fab1adfa9e8fdfbbb14fd7f
nikita <nikita.derjugin@gmail.com>
2023-12-06 12:52:07 +0200

    refactor 2

--- src/components/ui/modal/modal.tsx
+++ src/components/ui/modal/modal.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from 'react'
+import { ComponentPropsWithoutRef, ElementRef, ReactNode, forwardRef } from 'react'
 
 import XMark from '@/assets/icons/XMark'
 import { Button } from '@/components/ui/button'
@@ -16,55 +16,61 @@ type ModalPropsType = {
   isOpen?: boolean
   onOpenChange: () => void
   title?: string
-}
+} & ComponentPropsWithoutRef<typeof Dialog.Root>
 
-const Modal = ({
-  bodyText,
-  bodyTextScroll,
-  children,
-  closeIcon,
-  isOpen,
-  onOpenChange,
-  title,
-}: ModalPropsType) => {
-  return (
-    <Dialog.Root onOpenChange={onOpenChange} open={isOpen}>
-      <Dialog.Trigger asChild>
-        <Button variant={'primary'}>Open modal</Button>
-      </Dialog.Trigger>
-      <Dialog.Portal>
-        <Dialog.Overlay className={s.DialogOverlay} />
-        <Dialog.Content className={s.DialogContent}>
-          {title && (
-            <div className={s.ContentHeader}>
-              <Dialog.Title className={s.DialogTitle}>
-                <Typography variant={'h2'}>{title}</Typography>
-              </Dialog.Title>
-            </div>
-          )}
-          {title && closeIcon && (
-            <Dialog.Close asChild>
-              <button aria-label={'Close'} className={s.IconButton}>
-                {<XMark size={1.5} />}
-              </button>
-            </Dialog.Close>
-          )}
-          {bodyText && bodyTextScroll ? (
-            <div className={s.BodyTextScroll}>
-              <ScrollArea text={bodyText} />
-            </div>
-          ) : (
-            bodyText && (
-              <div className={s.BodyText}>
-                <Typography variant={'body1'}>{bodyText}</Typography>
+const Modal = forwardRef<ElementRef<typeof Dialog.Root>, ModalPropsType>(
+  (
+    {
+      bodyText,
+      bodyTextScroll,
+      children,
+      closeIcon,
+      isOpen = false,
+      onOpenChange,
+      title,
+      ...restProps
+    },
+    ref
+  ) => {
+    return (
+      <Dialog.Root onOpenChange={onOpenChange} open={isOpen}>
+        <Dialog.Trigger asChild>
+          <Button variant={'primary'}>Open modal</Button>
+        </Dialog.Trigger>
+        <Dialog.Portal>
+          <Dialog.Overlay className={s.DialogOverlay} />
+          <Dialog.Content className={s.DialogContent}>
+            {title && (
+              <div className={s.ContentHeader}>
+                <Dialog.Title className={s.DialogTitle}>
+                  <Typography variant={'h2'}>{title}</Typography>
+                </Dialog.Title>
               </div>
-            )
-          )}
-          {children && <div className={s.ChildrenContainer}>{children}</div>}
-        </Dialog.Content>
-      </Dialog.Portal>
-    </Dialog.Root>
-  )
-}
+            )}
+            {title && closeIcon && (
+              <Dialog.Close asChild>
+                <button aria-label={'Close'} className={s.IconButton}>
+                  {<XMark size={1.5} />}
+                </button>
+              </Dialog.Close>
+            )}
+            {bodyText && bodyTextScroll ? (
+              <div className={s.BodyTextScroll}>
+                <ScrollArea text={bodyText} />
+              </div>
+            ) : (
+              bodyText && (
+                <div className={s.BodyText}>
+                  <Typography variant={'body1'}>{bodyText}</Typography>
+                </div>
+              )
+            )}
+            {children && <div className={s.ChildrenContainer}>{children}</div>}
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
+    )
+  }
+)
 
 export default Modal

7ae239180e088ce6eef70efbbba3c8dba49ee897
Yuliya Martinchuk <yuliyamartinchuk@gmail.com>
2023-12-09 22:50:57 +0300

    feat: add forwardRef

--- src/components/ui/dropDownMenu/dropDownMenu.tsx
+++ src/components/ui/dropDownMenu/dropDownMenu.tsx
@@ -1,23 +1,24 @@
-import { ComponentPropsWithoutRef, ReactNode } from 'react'
+import { ComponentPropsWithoutRef, ElementRef, forwardRef, ReactNode } from 'react'
 
 import * as DropdownMenuRadix from '@radix-ui/react-dropdown-menu'
-
 import s from './dropDownMenu.module.scss'
 
-type Props = {
+type DropdownProps = {
   children: ReactNode
   trigger: ReactNode
 } & ComponentPropsWithoutRef<typeof DropdownMenuRadix.Root>
 
-export const DropDownMenu = ({ children, trigger, ...rest }: Props) => {
-  return (
-    <DropdownMenuRadix.Root {...rest}>
-      <DropdownMenuRadix.Trigger>{trigger}</DropdownMenuRadix.Trigger>
-      <DropdownMenuRadix.Portal>
-        <DropdownMenuRadix.Content align={'end'} className={s.dropdownMenuContent}>
-          {children}
-        </DropdownMenuRadix.Content>
-      </DropdownMenuRadix.Portal>
-    </DropdownMenuRadix.Root>
-  )
-}
+export const DropDownMenu = forwardRef<ElementRef<typeof DropdownMenuRadix.Root>, DropdownProps>(
+  ({ children, trigger, ...rest }, ref) => {
+    return (
+      <DropdownMenuRadix.Root {...rest}>
+        <DropdownMenuRadix.Trigger>{trigger}</DropdownMenuRadix.Trigger>
+        <DropdownMenuRadix.Portal>
+          <DropdownMenuRadix.Content ref={ref} align={'end'} className={s.dropdownMenuContent}>
+            {children}
+          </DropdownMenuRadix.Content>
+        </DropdownMenuRadix.Portal>
+      </DropdownMenuRadix.Root>
+    )
+  }
+)

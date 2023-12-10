402e62dd48bd515b515296f72b095aba9264ad6b
Yuliya Martinchuk <yuliyamartinchuk@gmail.com>
2023-12-10 10:40:43 +0300

    feat: add forwardRef for DropDownItem component

--- src/components/ui/dropDownMenu/dropDownItem.tsx
+++ src/components/ui/dropDownMenu/dropDownItem.tsx
@@ -1,12 +1,17 @@
-import { ComponentPropsWithoutRef } from 'react'
+import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react'
 
 import * as DropdownMenuRadix from '@radix-ui/react-dropdown-menu'
 import { clsx } from 'clsx'
 
 import s from './dropDownMenu.module.scss'
 
-type Props = ComponentPropsWithoutRef<typeof DropdownMenuRadix.Item>
+type DropDownItemProps = ComponentPropsWithoutRef<typeof DropdownMenuRadix.Item>
 
-export const DropDownItem = ({ className, ...rest }: Props) => {
-  return <DropdownMenuRadix.Item className={clsx(s.dropdownMenuItem, className)} {...rest} />
-}
+export const DropDownItem = forwardRef<
+  ElementRef<typeof DropdownMenuRadix.Item>,
+  DropDownItemProps
+>(({ className, ...rest }, ref) => {
+  return (
+    <DropdownMenuRadix.Item ref={ref} className={clsx(s.dropdownMenuItem, className)} {...rest} />
+  )
+})

c6ed3643d0c63bdd21d9f5dc51341d3ca69e2ab7
Yuliya Martinchuk <yuliyamartinchuk@gmail.com>
2023-12-10 11:09:13 +0300

    feat: add Event handler and other props for DropDownItem component

--- src/components/ui/dropDownMenu/dropDownItem.tsx
+++ src/components/ui/dropDownMenu/dropDownItem.tsx
@@ -1,17 +1,35 @@
-import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react'
+import { ComponentPropsWithoutRef, ElementRef, forwardRef, ReactNode } from 'react'
 
 import * as DropdownMenuRadix from '@radix-ui/react-dropdown-menu'
 import { clsx } from 'clsx'
 
 import s from './dropDownMenu.module.scss'
 
-type DropDownItemProps = ComponentPropsWithoutRef<typeof DropdownMenuRadix.Item>
+type DropDownItemProps = {
+  children?: ReactNode
+  className?: string
+  onSelect: (event: Event) => void
+} & ComponentPropsWithoutRef<typeof DropdownMenuRadix.Item>
 
 export const DropDownItem = forwardRef<
   ElementRef<typeof DropdownMenuRadix.Item>,
   DropDownItemProps
->(({ className, ...rest }, ref) => {
+>(({ children, onSelect, className, ...rest }, ref) => {
+  const DropDownItemClassName = clsx(s.dropdownMenuItem, className)
+
+  const onSelectHandler = (e: Event) => {
+    onSelect && onSelect(e)
+    e.preventDefault()
+  }
+
   return (
-    <DropdownMenuRadix.Item ref={ref} className={clsx(s.dropdownMenuItem, className)} {...rest} />
+    <DropdownMenuRadix.Item
+      ref={ref}
+      className={DropDownItemClassName}
+      onSelect={onSelectHandler}
+      {...rest}
+    >
+      {children}
+    </DropdownMenuRadix.Item>
   )
 })

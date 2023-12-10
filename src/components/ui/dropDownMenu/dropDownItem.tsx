312dfc509b1ae2298d2f65d5f21787ac8a21a52c
Yuliya Martinchuk <yuliyamartinchuk@gmail.com>
2023-12-10 11:27:00 +0300

    feat: add DropDownItemWithIcon component

--- src/components/ui/dropDownMenu/dropDownItem.tsx
+++ src/components/ui/dropDownMenu/dropDownItem.tsx
@@ -6,6 +6,7 @@ import { motion } from 'framer-motion'
 
 import s from './dropDownMenu.module.scss'
 import { dropdownAnimations } from '@/components/ui/dropDownMenu/dropdownMenuAnimations'
+import { Typography } from '@/components/ui/typography'
 
 type DropDownItemProps = {
   children?: ReactNode
@@ -32,6 +33,40 @@ export const DropDownItem = forwardRef<
       {...rest}
     >
       <motion.div {...dropdownAnimations.item}>{children}</motion.div>
+    </DropdownMenuRadix.Item>
+  )
+})
+
+type DropDownItemWithIconProps = Omit<DropDownItemProps, 'children'> & {
+  icon: ReactNode
+  text: string
+} & ComponentPropsWithoutRef<typeof DropdownMenuRadix.Item>
+
+export const DropDownItemWithIcon = forwardRef<
+  ElementRef<typeof DropdownMenuRadix.Item>,
+  DropDownItemWithIconProps
+>(({ children, onSelect, className, icon, text, ...rest }, ref) => {
+  const classNames = {
+    item: clsx(s.item, className),
+    itemIcon: s.itemIcon,
+  }
+
+  const onSelectHandler = (e: Event) => {
+    onSelect && onSelect(e)
+    e.preventDefault()
+  }
+
+  return (
+    <DropdownMenuRadix.Item
+      ref={ref}
+      className={classNames.item}
+      onSelect={onSelectHandler}
+      {...rest}
+    >
+      <motion.div {...dropdownAnimations.item}>
+        <div className={classNames.itemIcon}>{icon}</div>
+        <Typography variant={'caption'}>{text}</Typography>
+      </motion.div>
     </DropdownMenuRadix.Item>
   )
 })

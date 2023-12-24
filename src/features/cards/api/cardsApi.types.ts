8fff055e5f9fefb008f506395b34245947ecd47d
Yuliya Martinchuk <yuliyamartinchuk@gmail.com>
2023-12-24 23:48:59 +0300

    feat: add type for Card

---
+++ src/features/cards/api/cardsApi.types.ts
@@ -0,0 +1,15 @@
+export type Card = {
+  answer: string
+  answerImg: null | string
+  answerVideo: string
+  created: string
+  deckId: string
+  grade: number
+  id: string
+  question: string
+  questionImg: null | string
+  questionVideo: string
+  shots: number
+  updated: string
+  userId: string
+}

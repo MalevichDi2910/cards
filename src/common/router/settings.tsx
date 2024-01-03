bfa941a7c060589b60da2c90c577b5d621db4236
MalevichDi2910 <malevicdiana@gmail.com>
2024-01-03 19:50:20 +0200

    add elements SignInForm, PersonalInfo, Decks

--- src/common/router/settings.tsx
+++ src/common/router/settings.tsx
@@ -1,15 +1,23 @@
 import { RouteObject } from 'react-router-dom'
 
+import { SignInForm } from '@/features/SignInForm'
+import { PersonalInfo } from '@/features/personal-information'
+import { Decks } from '@/pages/decks'
+
 export const publicRoutes: RouteObject[] = [
   {
-    element: <div>SignInPage</div>,
+    element: <SignInForm />,
     path: '/sign-in',
   },
 ]
 
 export const privateRoutes: RouteObject[] = [
   {
-    element: <div>ProfilePage</div>,
+    element: <PersonalInfo user={{ email: 'maldi-999@mail.ru', name: 'Diana', src: '' }} />,
     path: '/profile',
+  },
+  {
+    element: <Decks />,
+    path: '/decks',
   },
 ]

7f1b32ffea5e869157b593c8264a08808c461fa3
MalevichDi2910 <malevicdiana@gmail.com>
2024-01-15 20:29:59 +0200

    feat: added decksSlice for store

--- src/common/services/store/store.ts
+++ src/common/services/store/store.ts
@@ -2,6 +2,7 @@ import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 
 import { baseApi } from '@/common/services/api/baseApi'
 import { cardSlice } from '@/features/cards/modal/cardsSlice'
+import { decksSlice } from '@/features/decks/modal'
 import { configureStore } from '@reduxjs/toolkit'
 
 export const store = configureStore({
@@ -9,6 +10,7 @@ export const store = configureStore({
   reducer: {
     [baseApi.reducerPath]: baseApi.reducer,
     [cardSlice.reducerPath]: cardSlice.reducer,
+    [decksSlice.reducerPath]: decksSlice.reducer,
   },
 })
 

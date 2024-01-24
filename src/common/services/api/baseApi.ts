307f02f9b022833ee836d17e1b26f14223f56d32
MalevichDi2910 <malevicdiana@gmail.com>
2024-01-24 12:55:03 +0200

    feat: added function retry, refetchOnFocus and refetchOnReconnect

--- src/common/services/api/baseApi.ts
+++ src/common/services/api/baseApi.ts
@@ -1,14 +1,19 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery, retry } from '@reduxjs/toolkit/query/react'
 
 export const baseApi = createApi({
-  baseQuery: fetchBaseQuery({
-    baseUrl: 'https://api.flashcards.andrii.es',
-    credentials: 'include',
-    prepareHeaders: headers => {
-      headers.append('x-auth-skip', 'true')
-    },
-  }),
+  baseQuery: retry(
+    fetchBaseQuery({
+      baseUrl: 'https://api.flashcards.andrii.es',
+      credentials: 'include',
+      prepareHeaders: headers => {
+        headers.append('x-auth-skip', 'true')
+      },
+    }),
+    { maxRetries: 3 }
+  ),
   endpoints: () => ({}),
   reducerPath: 'baseApi',
+  refetchOnFocus: true,
+  refetchOnReconnect: true,
   tagTypes: ['Decks', 'Me'],
 })

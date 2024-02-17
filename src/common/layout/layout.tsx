7c5c97f23c08ad50b7bf8541d575be5abf065b15
Yuliya Martinchuk <yuliyamartinchuk@gmail.com>
2024-02-17 16:25:44 +0300

    fix: delete unuseful code

--- src/common/layout/layout.tsx
+++ src/common/layout/layout.tsx
@@ -1,17 +1,9 @@
-import { Outlet, useOutletContext } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 
 import { Header } from '@/components/ui/header'
 import { Loader } from '@/components/ui/loader'
 import { useLogoutMutation, useMeQuery } from '@/features/auth/api'
-
-type AuthContext = {
-  isAuthenticated: boolean
-}
-
-export function useAuthContext() {
-  return useOutletContext<AuthContext>()
-}
 
 export const Layout = () => {
   const { data, isError, isLoading } = useMeQuery()

8efdf19c852eebebd3bc335457453cc260f21ed8
Yuliya Martinchuk <yuliyamartinchuk@gmail.com>
2024-04-25 01:08:33 +0300

    style: add style for NotFoundPage component

--- src/pages/notFoundPage/notFoundPage.tsx
+++ src/pages/notFoundPage/notFoundPage.tsx
@@ -4,16 +4,18 @@ import { NotFound } from '@/assets/icons/notFound'
 import { Button } from '@/components/ui/button'
 import { Typography } from '@/components/ui/typography'
 
+import s from './notFoundPage.module.scss'
+
 export const NotFoundPage = () => {
   return (
-    <div>
-      <div>
+    <div className={s.container}>
+      <div className={s.image}>
         <NotFound height={192} width={451} />
       </div>
-      <Typography as={'span'} variant={'body1'}>
+      <Typography as={'span'} className={s.text} variant={'body1'}>
         Sorry! Page not found!
       </Typography>
-      <Button as={Link} to={'/'}>
+      <Button as={Link} className={s.button} to={'/'}>
         <Typography as={'span'} variant={'subtitle2'}>
           Back to home page
         </Typography>
